6b0aef08929696f0dcb983f5507d69e590a7ffec
lokeswarar <144889290+lokeswarar@users.noreply.github.com>
2023-09-13 13:47:57 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
+  ami           = "ami-04cb4ca688797756f"
   instance_type = "t2.micro"
 
   tags = {
